9783cfe71965125b04805b011eecfd993f6a04a1
kyuya11 <83804045+kyuya11@users.noreply.github.com>
2022-10-14 11:59:44 +0900

    オセロのシーン設定

---
+++ Code/Scene/Othello/Scene_Othello.cpp
@@ -0,0 +1,17 @@
+#include "./../Code/Scene/Othello/Scene_Othello.h"
+
+Scene_Othello::Scene_Othello() {
+
+}
+
+Scene_Othello::~Scene_Othello() {
+
+}
+
+void Scene_Othello::Update() {
+
+}
+
+void Scene_Othello::Draw() {
+
+}
---
+++ Code/Scene/Othello/Scene_Othello.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "./../Scene.h"
+
+class Scene_Othello : public Scene {
+private:
+
+public:
+	Scene_Othello();
+	~Scene_Othello();
+	void Update();
+	void Draw();
+};
