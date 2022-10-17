e6ec0b3508bd5fb3fbe64f52a6d122ac0c688c24
netainemui <92067870+netainemui@users.noreply.github.com>
2022-10-17 09:57:30 +0900

    画像を表示させたい

--- Code/Scene/daifugou/CP_Player.cpp
+++ Code/Scene/daifugou/CP_Player.cpp
@@ -1,4 +1,7 @@
 #include"DxLib.h"
 #include"CP_Player.h"
 #include"./../Code/GetKey.h"
-
+//‘å•x‹ƒvƒŒƒCƒ„[
+void CareerPoker::CP_Player_Initialize(){
+	rule = LoadGraph("Code/Scene/daifugou/images/rule.png");
+}
--- Code/Scene/daifugou/CP_Player.h
+++ Code/Scene/daifugou/CP_Player.h
@@ -1,1 +1,12 @@
 #pragma once
+
+class CareerPoker {
+private:
+	int rule;//ルールの画像
+	int stage;//ステージの画像
+public:
+	void CP_Player_Initialize();
+	void CP_Player_Finalize();
+	void CP_Player_Update();
+	void CP_Player_Draw();
+};
