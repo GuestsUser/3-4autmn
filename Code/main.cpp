777efe234196169c45f988d08444797e87cc04b7
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2022-10-17 09:58:59 +0900

    タイトルセレクト基盤作成

--- Code/main.cpp
+++ Code/main.cpp
@@ -6,7 +6,7 @@
 
 #include "DxLib.h"
 #include "ConstVal.h"
-#include "./Scene/Sample/Title.h"
+#include "./Scene/Title/Scene_GameTitle.h"
 #include "GetKey.h"
 #include "SceneManager.h"
 #include "Worldval.h"
@@ -33,7 +33,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	GlovalLoading(); //GlovalLoading.cppの関数、WorldVal::Getで取り出せる値の用意を行うサンプル
 
 	key = new KeySystem(); //キー入力受付用クラスの実体作成
-	SceneManager* scm = new SceneManager(new Title()); //引数に最初に実行したいシーン実体を入れる
+	SceneManager* scm = new SceneManager(new Scene_GameTitle()); //引数に最初に実行したいシーン実体を入れる
 
 	while (ProcessMessage() == 0 && (!CheckHitKey(KEY_INPUT_ESCAPE)) && key->GetKeyState(SELECT_KEY) != KEY_PUSH) { //GetKeyシステム使用例、backボタンが押された瞬間にfalseとなる
 		//_RPTF1(_CRT_WARN, "%s\n", "test"); //デバッグ表示
