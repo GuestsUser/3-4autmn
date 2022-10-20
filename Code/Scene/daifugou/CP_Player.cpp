418cc19faeee6fbcd6a638e0118840da69d06ed0
netainemui <92067870+netainemui@users.noreply.github.com>
2022-10-20 15:01:34 +0900

    大富豪の背景

--- Code/Scene/daifugou/CP_Player.cpp
+++ Code/Scene/daifugou/CP_Player.cpp
@@ -3,5 +3,18 @@
 #include"./../Code/GetKey.h"
 //‘å•x‹ƒvƒŒƒCƒ„[
 void CareerPoker::CP_Player_Initialize(){
-	rule = LoadGraph("Code/Scene/daifugou/images/rule.png");
+	//rule = LoadGraph("Code/Scene/daifugou/images/rule.png");//ƒ‹[ƒ‹‰æ‘œ
+	stage = LoadGraph("Code/Scene/daifugou/images/CareerPoker.png");//”wŒi‰æ‘œ
 }
+
+void CareerPoker::CP_Player_Finalize() {
+	DeleteGraph(stage);
+}
+void CareerPoker::CP_Player_Update() {
+
+}
+
+void CareerPoker::CP_Player_Draw() {
+	DrawRotaGraph(640, 360, 1.0 , 0, stage, TRUE);
+}
+
---
+++ Code/Scene/daifugou/CP_Scene.cpp
@@ -0,0 +1,17 @@
+#include"CP_Scene.h"
+#include"CP_Player.h"
+
+CP_Scene::CP_Scene(){
+	Player = new CareerPoker();
+	Player->CP_Player_Initialize();
+}
+CP_Scene::~CP_Scene() {
+	delete Player;
+}
+void CP_Scene::Update() {
+	Player->CP_Player_Update();
+}
+
+void CP_Scene::Draw() {
+	Player->CP_Player_Draw();
+}
