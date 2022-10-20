2280bf269f5d8f41b43267d1b91a974f2729bb8a
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-10-20 09:31:47 +0900

    カードの情報をオブジェクト化しました

--- Code/Scene/Asuma/PageOne.cpp
+++ Code/Scene/Asuma/PageOne.cpp
@@ -5,7 +5,17 @@
 #include "./../../Worldval.h"
 
 void PageOne::PageOne_Initialize() {
-	LoadDivGraph("./../Resource/image/toranpu_all.png", 65, 5, 13, 200, 300, card[5][13]);
+	LoadDivGraph("./../Resource/image/toranpu_all.png", 65, 5, 13, 200, 300, card_type);
+
+	// iはスート、jはナンバー
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 13; j++) {
+			Card_obj[i + j] = Card(card_type[i + j], j, i);
+		}
+	}
+
+	Card_obj[52] = Card(card_type[52], 0, 5);
+	Card_obj[53] = Card(card_type[53], 99, 5);
 }
 
 void PageOne::PageOne_Finalize() {
--- Code/Scene/Asuma/PageOne.h
+++ Code/Scene/Asuma/PageOne.h
@@ -1,8 +1,24 @@
 #pragma once
 
+class Card {
+private:
+	int img;	//画像用の変数
+	int num;	//カードの数字
+	int suit;	//カードのスート(マーク)
+
+public:
+	Card(int i, int n, int s) {
+		img = i;
+		num = n;
+		suit = s;
+	}
+};
+
 class PageOne {
 private:
-	int *card[5][13];
+	Card Card_obj[54];	//カードの情報
+
+	int card_type[65];	//画像用ハンドル
 
 	int Mouse_X; //マウスのX座標
 	int Mouse_Y; //マウスのY座標
