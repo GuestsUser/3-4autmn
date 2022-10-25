1925142b06275c10559656b2e324f057dd614a06
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-10-25 15:02:20 +0900

    表示できるようになった

--- Code/Scene/Karuta/Karu_Game.cpp
+++ Code/Scene/Karuta/Karu_Game.cpp
@@ -1,12 +1,28 @@
-#include"Karu_Game.h"
 #include"DxLib.h"
 #include"./../Code/GetKey.h"
+#include"Karu_Game.h"
 
 void Karu_Game::Karu_Game_Initialize() {
+	Karu_Bg = LoadGraph("Resource/image/Karu_Tatami.png");
+	LoadDivGraph("Resource/image/Karu_Fuda.png",100,10,10,Karu_imgX,Karu_imgY,*Karu_fuda,TRUE);
+
 }
 void Karu_Game::Karu_Game_Finalize() {
+	DeleteGraph(Karu_Bg);
+
 }
 void Karu_Game::Karu_Game_Update() {
+
 }
 void Karu_Game::Karu_Game_Draw() {
+	DrawRotaGraph(640, 360, 1.0, 0, Karu_Bg, TRUE);
+	
+	for (float j = Karu_StartPosY; j < (Karu_imgY * Karu_MaxY); j += (Karu_imgY * Karu_Y) + Karu_Space) {
+		for (float i = Karu_StartPosX; i < (Karu_imgX * Karu_MaxX); i += (Karu_imgX * Karu_X) + Karu_Space) {
+
+			DrawRotaGraph(i, j, 1.0, 0, Karu_fuda[4][6], TRUE);
+			Karu_X++;
+		}
+		Karu_Y++;
+	}
 }
--- Code/Scene/Karuta/Karu_Game.h
+++ Code/Scene/Karuta/Karu_Game.h
@@ -2,12 +2,24 @@
 
 class Karu_Game{
 private:
-	int Karu_Bg;	//ƒQ[ƒ€”wŒi
-	int Karu_fuda;	//‚©‚é‚½‚ÌD‰æ‘œŠi”[—p
+	int Karu_Bg;				//ƒQ[ƒ€”wŒi
+	int Karu_fuda[10][10];		//‚©‚é‚½‚ÌD‰æ‘œŠi”[—p
+
+	int Karu_Efuda[3][5];		//‚©‚é‚½‚ÌŠGD”z’u—p
+	int Karu_MaxX = 5, Karu_MaxY = 3;	//•\¦‚³‚¹‚éŠGD‚Ì–‡”
+
+	float Karu_StartPosX = 75.0f;		//ŠGD‚Ì”z’uŠJnˆÊ’u
+	float Karu_StartPosY = 102.5f;		//..
+	float Karu_imgX = 150.0f;	//‚©‚é‚½‚Ì‰æ‘œ‚Ì‘å‚«‚³
+	float Karu_imgY = 205.0f;	//..
+
+	float Karu_Space = 10.0f;	//ŠGD‚ÆŠGD‚ÌŠÔ
 
 public:
 	void Karu_Game_Initialize();
 	void Karu_Game_Finalize();
 	void Karu_Game_Update();
 	void Karu_Game_Draw();
+
+	int Karu_X, Karu_Y;	//•\¦‚³‚¹‚éŠGD‚Ì–‡”
 };
