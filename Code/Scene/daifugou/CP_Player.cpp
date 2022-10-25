9050678303554613698dd680555aadcd78ab835b
netainemui <92067870+netainemui@users.noreply.github.com>
2022-10-25 15:01:00 +0900

    トランプの画像

--- Code/Scene/daifugou/CP_Player.cpp
+++ Code/Scene/daifugou/CP_Player.cpp
@@ -5,6 +5,7 @@
 void CareerPoker::CP_Player_Initialize(){
 	//rule = LoadGraph("Code/Scene/daifugou/images/rule.png");//ƒ‹[ƒ‹‰æ‘œ
 	stage = LoadGraph("Code/Scene/daifugou/images/CareerPoker.png");//”wŒi‰æ‘œ
+	//LoadDivGraph("Code/Scene/daifgou/images/ playingcards.png",65,5,13,16,16, cards[0][0],TRUE);
 }
 
 void CareerPoker::CP_Player_Finalize() {
@@ -16,5 +17,6 @@ void CareerPoker::CP_Player_Update() {
 
 void CareerPoker::CP_Player_Draw() {
 	DrawRotaGraph(640, 360, 1.0 , 0, stage, TRUE);
+	//DrawRotaGraph(640, 360, 1.0, 0, cards[2], TRUE);
 }
 
--- Code/Scene/daifugou/CP_Player.h
+++ Code/Scene/daifugou/CP_Player.h
@@ -2,8 +2,10 @@
 
 class CareerPoker {
 private:
+	int i, y;
 	int rule;//ルールの画像
 	int stage;//ステージの画像
+	int cards[5][13];//トランプ画像
 public:
 	void CP_Player_Initialize();
 	void CP_Player_Finalize();
