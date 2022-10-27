1624d04e2405f454fa367158d5407a0f91c29d06
netainemui <92067870+netainemui@users.noreply.github.com>
2022-10-27 12:11:00 +0900

    トランプ描画だけ

--- Code/Scene/daifugou/CP_Player.cpp
+++ Code/Scene/daifugou/CP_Player.cpp
@@ -5,18 +5,21 @@
 void CareerPoker::CP_Player_Initialize(){
 	//rule = LoadGraph("Code/Scene/daifugou/images/rule.png");//ƒ‹[ƒ‹‰æ‘œ
 	stage = LoadGraph("Code/Scene/daifugou/images/CareerPoker.png");//”wŒi‰æ‘œ
-	//LoadDivGraph("Code/Scene/daifgou/images/ playingcards.png",65,5,13,16,16, cards[0][0],TRUE);
+	LoadDivGraph("Resource/image/toranpu_all.png",53,13,5,200,300, *cards,TRUE);
+	for (int i = 0; i < 4; i++) {
+		printf("‘«‚µZ");
+	}
 }
 
 void CareerPoker::CP_Player_Finalize() {
 	DeleteGraph(stage);
 }
 void CareerPoker::CP_Player_Update() {
-
+	
 }
 
 void CareerPoker::CP_Player_Draw() {
 	DrawRotaGraph(640, 360, 1.0 , 0, stage, TRUE);
-	//DrawRotaGraph(640, 360, 1.0, 0, cards[2], TRUE);
+	DrawRotaGraph(100, 200, 0.5, 0, cards[0][0], TRUE);
 }
 
