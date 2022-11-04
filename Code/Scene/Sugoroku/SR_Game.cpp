872e865f53727f9366bea0387214f6d5ec393de7
YUTOKUBA <yuuto-kuba-20h@stu.kbc.ac.jp>
2022-11-04 09:24:50 +0900

    no message

--- Code/Scene/Sugoroku/SR_Game.cpp
+++ Code/Scene/Sugoroku/SR_Game.cpp
@@ -1,13 +1,14 @@
 #include "./../../GlovalLoading.h"	
 #include "./../../GetKey.h"
 #include "./../../SceneManager.h"
-#include "MapLoad.h"
+#include "SR_Map.h"
 #include "SR_Game.h"
 #include "SR_Saikoro.h"
 
 SR_Game::SR_Game() {
 	SR_MAP = LoadGraph("Resource/image/SR_MAP.png");
 	saikoro = new SR_Saikoro();
+	map = new SR_Map();
 }
 
 void SR_Game::Update() {
@@ -17,4 +18,5 @@ void SR_Game::Update() {
 void SR_Game::Draw() {
 	DrawGraph(0, 0, SR_MAP,false);
 	saikoro->Draw();
+	map ->Draw();
 }
---
+++ Code/Scene/Sugoroku/SR_Map.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "./../Scene.h"
+class SR_Map {
+private:
+	int MassX[65];	/*マスの総数*/
+	int MassY[65];	/*マスの総数*/
+	int Player1;
+	int Player2;
+	int x, y;
+public:
+	SR_Map();
+	void Update();
+	void Draw();
+};
