4248bc0d04889576312acde0fa47b1629d02681a
netainemui <92067870+netainemui@users.noreply.github.com>
2022-11-10 09:24:42 +0900

    hanntei

--- Code/Scene/daifugou/CP_Player.cpp
+++ Code/Scene/daifugou/CP_Player.cpp
@@ -9,7 +9,7 @@ void CareerPoker::CP_Player_Initialize(){
 	//rule = LoadGraph("Code/Scene/daifugou/images/rule.png");//ルール画像
 	stage = LoadGraph("Resource/image/CareerPoker.png");//背景画像
 	LoadDivGraph("Resource/image/toranpu_all.png",53,13,5,200,300, card_type,TRUE);
-	for (i = 0; i < 5; i++) {
+	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 51; j++) {
 			Card_obj.push_back(C(card_type[i + j], j, i));
 		}
@@ -18,15 +18,26 @@ void CareerPoker::CP_Player_Initialize(){
 	Card_joker = C(card_type[53], 99, 5);	//ジョーカー
 
 	for (i = 0; i < 13; i++) {
-		r = GetRand(sizeof(Card_obj));
+		r = GetRand(Card_obj.size());
 		Player_card.push_back(Card_obj[r]);
 		Card_obj.erase(Card_obj.begin() + r);
 	}
+
+
+	Player_X = 150;
+	Player_Y = 575;
+}
+void CareerPoker::onesec(void) {
+	time_t t1;
+	srand(time(NULL));
+
+	t1 = time(NULL);
+	while (1 != time(NULL) - t1);
 }
 
 void CareerPoker::CP_Player_Finalize() {
 	DeleteGraph(stage);
-	for (i = 0; i < 54; i++) {
+	for (i = 0; i < 53; i++) {
 		DeleteGraph(card_type[i]);
 	}
 }
@@ -36,17 +47,49 @@ void CareerPoker::deckmake() {
 	for (i = 0; i <= 53; i++)
 		T_cards[i] = i + 1;
 }
-
+int Color;
 void CareerPoker::CP_Player_Update() {
-	
+	GetMousePoint(&Mouse_X, &Mouse_Y);
+
+	for (n = 0; n < 13; n++) {
+		//if(C::Hit(Mouse_X, Mouse_Y, Player_X + (n%13) * (card_w * 0.3), Player_Y + (card_h * 0.3), card_w, card_h, 0.3)){
+		if(Mouse_X>100 && Mouse_X < 200 && Mouse_Y > 100 && Mouse_Y < 200){
+			if (key->GetKeyState(REQUEST_MOUSE_LEFT) == KEY_PUSH) {
+				r = GetRand(Card_obj.size());
+				Player_card.push_back(Card_obj[r]);
+				Card_obj.erase(Card_obj.begin() + r);
+				n = 0;
+			}
+			Color = GetColor(255, 0, 0);
+
+		}
+		else {
+			Color = GetColor(255, 255, 255);
+
+		}
+		switch (i)
+		{
+		default:
+			break;
+		}
+
+	}
+	/*if (C::Hit(Mouse_X, Mouse_Y, Player_X, Player_Y, card_w, card_h)) {
+		j = GetColor(255, 0, 0);
+	}
+	else {
+		j = GetColor(0, 0, 255);
+	}*/
 }
 
 void CareerPoker::CP_Player_Draw() {
 	DrawRotaGraph(640, 360, 1.0 , 0, stage, TRUE);
+	
 	int i = 0;
 	for (auto itr = Player_card.begin(); itr != Player_card.end(); itr++) {
-		DrawRotaGraph(100 + i * 45, 500, 0.3, 0, (*itr).img, TRUE);
+		DrawRotaGraph(Player_X + i * (card_w * 0.3), Player_Y + (card_h * 0.3), 0.3, 0, (*itr).img, TRUE);
 		i++;
 	}
+	DrawBox(100, 100, 200, 200, Color, TRUE);
 }
 
--- Code/Scene/daifugou/CP_Player.h
+++ Code/Scene/daifugou/CP_Player.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<random>
 #include<algorithm>
+#include<time.h>
 #include <vector>
 #include <list>
 
@@ -8,9 +9,20 @@ class C{
 public:
 	C() {}
 
+	int card_w = 200;	//カード１枚の幅
+	int card_h = 300;	//カード１枚の高さ
 	int img;	//画像用の変数
 	int num;	//カードの数字
 	int suit;	//カードのスート(マーク)
+
+	bool Hit(int mx, int my, int cx, int cy, int cw, int ch, double ExRate) {
+		if (mx > cx - (cw * ExRate) / 2 && mx < cx + (cw * ExRate) / 2 && my > cy - (ch * ExRate) / 2 && my < cy + (ch * ExRate) / 2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
 	//Card(画像、数値、スート、使用フラグ)
 	C(int i, int n, int s) {
@@ -19,9 +31,10 @@ public:
 		suit = s;
 	}
 };
-class CareerPoker {
+class CareerPoker : public C{
 private:
 	int i, x, r, j;
+	int n;
 	int rule;//ルールの画像
 	int stage;//ステージの画像
 	int T_cards[54];//山札
@@ -30,7 +43,13 @@ private:
 	int order[4];//誰から始めるか
 	int pass;//ぱす
 	int rev;//革命
-
+	int Mouse_X;	//マウスのX座標
+	int Mouse_Y;    //マウスのY座標
+	int card_w = 200;
+	int card_h = 300;
+	int Player_X;
+	int Player_Y;
+	int player;
 	int card_type[54];	//画像用ハンドル
 
 	C Card_back;		//カードの裏面の情報
@@ -38,13 +57,13 @@ private:
 
 	//Card Card_obj[4][13];		//全カードの情報
 	std::vector<C> Card_obj;		//全カードの情報
-	std::list<C> Player_card;	//プレイヤーの持っているカードの情報
-	
+	std::vector<C> Player_card;	//プレイヤーの持っているカードの情報
+
 public:
 	void CP_Player_Initialize();
 	void CP_Player_Finalize();
 	void CP_Player_Update();
 	void CP_Player_Draw();
 	void deckmake();
-	
+	void onesec();
 };
