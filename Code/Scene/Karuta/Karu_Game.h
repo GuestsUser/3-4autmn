bfe22a567d4e5d0d24b7efd23b20993afd7db27b
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-11-17 14:38:04 +0900

    CPUを3体追加、画像を追加

--- Code/Scene/Karuta/Karu_Game.h
+++ Code/Scene/Karuta/Karu_Game.h
@@ -8,16 +8,30 @@
 #define KARU_MAX_X  5			//かるた配置の横の枚数
 #define KARU_MAX_Y  3			//..縦
 
-#define KARU_OTETUKI_MAX  2			//お手付きの制限
+#define HAND_ICON 2				//カーソルアイコンの画像最大数
+
+#define OTETUKI_IMG 2			//お手付き画像最大数
+#define KARU_OTETUKI_MAX  2		//お手付きの制限
+
+#define CLICK_MAX_TIME 60.0f	//クリック猶予
+#define TOUCH_MAX 1
+
+#define NUM_IMG_MAX 10			//数字画像最大数
+
+#define TOTAL_PLAYER 4			//参加人数
 
 class Karu_Game{
 private:
 	int Karu_Bg;				//ゲーム背景
 	int Karu_SideBg;			//ゲーム背景
-	int Karu_Otetuki_img[2];	//お手付き時の画像格納用
+	int Karu_Otetuki_img[OTETUKI_IMG];	//お手付き時の画像格納用
 	int Karu_getText;			//獲得枚数のテキスト画像格納用	
 	int Karu_PlayerText;		//プレイヤーのテキスト画像格納用
-	int Karu_numImg[10];		//数字のテキスト画像格納用
+	int Karu_numImg[NUM_IMG_MAX];		//数字のテキスト画像格納用
+
+	int Karu_CPU1_Text;		//CPU1のテキスト画像格納用
+	int Karu_CPU2_Text;		//CPU1のテキスト画像格納用
+	int Karu_CPU3_Text;		//CPU1のテキスト画像格納用
 
 	int Karu_fuda[KARU_IMAGE_MAX_X][KARU_IMAGE_MAX_Y];		//かるたの札画像格納用
 	int Fuda_voice[KARU_IMAGE_MAX_X][KARU_IMAGE_MAX_Y];		//読み上げ音声格納用
@@ -32,9 +46,13 @@ private:
 	int Mouse_X;	//マウスのX座標
 	int Mouse_Y;	//マウスのY座標
 
-	int Karu_Otetuki;
+	int Player_HandIcon[HAND_ICON];	//マウスカーソルアイコン格納用
+	int Enemy1_HandIcon[HAND_ICON];	//マウスカーソルアイコン格納用
+	int Enemy2_HandIcon[HAND_ICON];	//マウスカーソルアイコン格納用
+	int Enemy3_HandIcon[HAND_ICON];	//マウスカーソルアイコン格納用
 
 	bool end;
+
 
 public:
 	void Karu_Game_Initialize();
@@ -44,13 +62,20 @@ public:
 
 	void Efuda_Storage();
 	void Yomifuda_Storage();
+	void Voice_Storage();
+
 	void Mouse_HitBox();
+
+	void Click_Anim();
+	void Otetuki_Anim();
+
+	void Cpu_config();
+	void Cpu_reset();
+
+	void Player_Reset();
 
 	void Karu_GameOver();
 
-	void Otetuki_Anim();
-
-	int Karu_Efuda[KARU_MAX_Y][KARU_MAX_X];		//かるたの絵札配置用
 };
 
 struct EFUDA
@@ -84,8 +109,24 @@ extern struct FUDA Fuda[100];
 struct KARU_PLAYER
 {
 	int myFuda;	//取ってる枚数
-
-	int MousePoint[2];//マウスポインターの画像格納用
+	int Otetuki;
+	int Rank;
 };
 
 extern struct KARU_PLAYER Karu_player;
+
+struct KARU_CPU
+{
+	float x, y;
+	int myFuda;	//取ってる枚数
+	int setTime; 
+	int minTime;
+	int Time;
+	int Rank;
+
+	bool onClick;
+};
+
+extern struct KARU_CPU cpu_1;
+extern struct KARU_CPU cpu_2;
+extern struct KARU_CPU cpu_3;
