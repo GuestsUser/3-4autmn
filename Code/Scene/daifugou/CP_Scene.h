7e6bbfa65ff2deb25b735773881eb6dda5d67a68
netainemui <92067870+netainemui@users.noreply.github.com>
2022-12-02 15:05:59 +0900

    と..トランプさえできれば終わりなんです...

--- Code/Scene/daifugou/CP_Scene.h
+++ Code/Scene/daifugou/CP_Scene.h
@@ -2,10 +2,11 @@
 #include"./../Scene.h"
 
 class CareerPoker;
-
+class CP;
 class CP_Scene :public Scene {
 private:
-	CareerPoker* Player;
+	//CareerPoker* Player;
+	CP* Player;
 public:
 	CP_Scene();
 	~CP_Scene();
--- Code/Scene/daifugou/daifugou.h
+++ Code/Scene/daifugou/daifugou.h
@@ -1,29 +1,33 @@
-//#pragma once
-//
-//class CP {
-//private:
-//
-//
-//public:
-//	void onesec(void);
-//	void title(void);
-//	void deckmake();//山札生成
-//	void datareset(void);//データの設定
-//	void deckshuffle(void);//シャッフル
-//	void phase(int n);
-//	void turn(int n);
-//	void sort(int sort_x, int* sort_y, int sort_z);//ソートを決める
-//	void pl_routine(int n);
-//	void expend(int n);//手札が無くなった時
-//	void slash8(int n);//8切り用
-//	void main(void);
-//	void dealcards(void);
-//	void d3start(void);
-//	void status(int n);
-//	void print(int n);
-//	void revolution(void);
-//	void elevenback(void);
-//	void cp_routine(int n);
-//	void arrange(int n);
-//	void main(void);
-//};
+#pragma once
+
+class CP {
+private:
+	
+
+public:
+	void Player_Initialize();//済
+	void Player_Finalize();//済
+	//void CP_Player_Update();
+	void CP_Player_Draw();
+	void onesec();//済
+	void title();//済
+	void deckmake();//山札生成//済
+	void datareset();//データの設定
+	void deckshuffle();//シャッフル
+	void phase(int n);
+	void turn(int n);
+	void sort(int sort_x, int* sort_y, int sort_z);//ソートを決める
+	void pl_routine(int n);
+	void expend(int n);//手札が無くなった時
+	void slash8(int n);//8切り用
+	void main();
+	void dealcards();
+	void d3start();
+	void status(int n);
+	void print(int n);
+	void revolution();
+	void elevenback();
+	void cp_routine(int n);
+	void arrange(int n);
+	//void main(void);
+};
