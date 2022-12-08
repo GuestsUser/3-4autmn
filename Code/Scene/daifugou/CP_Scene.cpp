cb07a9dff616b318b7b280328e1047376dceca92
netainemui <92067870+netainemui@users.noreply.github.com>
2022-12-08 15:17:55 +0900

    完成できませんでした...

--- Code/Scene/daifugou/CP_Scene.cpp
+++ Code/Scene/daifugou/CP_Scene.cpp
@@ -2,32 +2,28 @@
 #include"CP_Player.h"
 #include"daifugou.h"
 CP_Scene::CP_Scene(){
-	//Player = new CareerPoker();
-	//Player->CP_Player_Initialize();
-	Player = new CP();
+	Player = new CareerPoker();
+	Player->CP_Player_Initialize();
+	/*Player = new CP();
 	Player->Player_Initialize();
-	
-	
-	
+	Player->onesec();//一秒数える
+	Player->deckmake();//デッキをセット
+	Player->deckshuffle();//デッキをシャッフル*/
 }
 CP_Scene::~CP_Scene() {
 	//delete Player;
 	delete Player;
 }
 void CP_Scene::Update() {
-	//Player->CP_Player_Update();
-	Player->onesec();//一秒数える
-	Player->deckmake();//デッキをセット
-	Player->deckshuffle();//デッキをシャッフル
-	Player->elevenback();//11バック
+	Player->CP_Player_Update();
+	/*Player->elevenback();//11バック
 	Player->datareset();//データをセット
 	Player->d3start();//ダイヤ3を持っている人からスタート
 	Player->dealcards();
-	Player->revolution();
+	Player->revolution();*/
 }
 
 void CP_Scene::Draw() {
-	//Player->title();//タイトル※こいつは消す
-	Player->Player_Draw();
+	Player->CP_Player_Draw();
 	/*Player->main();*/
 }
